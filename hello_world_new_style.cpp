8d9d0c6bfdaf5031a5f78233cc176706cb9ad9eb
Ubuntu <ubuntu@ip-172-31-27-134.ec2.internal>
2019-09-13 00:36:51 +0000

    program add

---
+++ hello_world_new_style.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2019 Wajd Mariam All rights reserved.
+//
+// Created by: Wajd Mariam  
+// Created on: September 2019
+// This is the "Hello, World!" program, but with some feautres added to it.
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hello,World!" << std::endl;
+}
